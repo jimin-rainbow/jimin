83fa211f2163cbd39b8420263d25acc76164d89e
ojm <oojjmm1994@naver.com>
2020-05-31 16:35:11 +0900

    add another info

---
+++ hello.py
@@ -0,0 +1,12 @@
+print("hello python")
+print("how are you?")
+print("I'm fine")
+
+print("what do you call a fish without an eye?")
+print("fsh")
+
+print("I'm cold")
+print("I'm very hungry")
+
+print("I like coffee")
+print("I love python")
